28c1712090bb13d69cab2588a4a0f2a4cd902148
abelavusau <abelavusau@gmail.com>
2015-09-26 16:45:33 +0300

    Db integration

---
+++ src/main/java/com/abelavusau/restfulapi/configuration/DbConfig.java
@@ -0,0 +1,45 @@
+package com.abelavusau.restfulapi.configuration;
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@PropertySource(value = "classpath:db.properties")
+public class DbConfig {
+	@Value("${driverClassName}")
+	private String driverClassName;
+	@Value("${url}")
+	private String url;
+	@Value("${username}")
+	private String username;
+	@Value("${password}")
+	private String password;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+
+		return dataSource;
+	}
+	
+	@Bean(name = "jdbcTemplate")
+	public JdbcTemplate getJdbcTemplate() {
+		return new JdbcTemplate(getDataSource());
+	}
+}
