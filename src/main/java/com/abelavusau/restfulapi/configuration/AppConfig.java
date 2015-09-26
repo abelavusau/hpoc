a3b8befd50a60a5f2cf2c4d19ce31172c129a4d3
abelavusau <abelavusau@gmail.com>
2015-09-26 21:05:04 +0300

    refactoring

--- src/main/java/com/abelavusau/restfulapi/configuration/AppConfig.java
+++ src/main/java/com/abelavusau/restfulapi/configuration/AppConfig.java
@@ -1,14 +1,9 @@
 package com.abelavusau.restfulapi.configuration;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -17,13 +12,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = { "com.abelavusau.restfulapi" })
 @Import(value = { DbConfig.class })
 public class AppConfig {
-	private static final MediaType APPLICATION_JSON = new MediaType("application", "json", StandardCharsets.UTF_8);
 
 	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		List<MediaType> supportedMediaTypes = new ArrayList<MediaType>(1);
-		supportedMediaTypes.add(APPLICATION_JSON);
-
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		return converter;
 	}
